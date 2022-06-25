b1bd5de8b6db567e998ffd9d270304c07eeb49e1
MBurtonshaw <56322461+MBurtonshaw@users.noreply.github.com>
2022-06-25 14:51:09 -0400

    Add files via upload

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDVnO-lVFQIWHPUVWsEGfQHGYWBDc92tJg",
+    authDomain: "clone-33ded.firebaseapp.com",
+    projectId: "clone-33ded",
+    storageBucket: "clone-33ded.appspot.com",
+    messagingSenderId: "349687602427",
+    appId: "1:349687602427:web:7900133390fbe12ba3e45e",
+    measurementId: "G-E9J9C8Y1YJ"
+  };
